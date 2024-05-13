08d5b5fce341355f24304437556500f3cc57404f
zhangyan <17746598041@163.com>
2024-05-13 18:29:25 +0800

    01-hello.asm

--- NASM/01-hello.asm
+++ NASM/01-hello.asm
@@ -1,5 +1,7 @@
 
-format elf
+; format elf64
+; nasm -f elf64 01-hello.asm -o hello.o
+; ld hello.o -o hello
 
 section .data
 	msg db "Hello World!", 0xA	; 定义字符串并以 0xA 换行符结束
@@ -10,13 +12,13 @@ section .text
 
 _start:
 	; 使用 Linux 系统调用来打印字符串
-	mov eax, 4			; 系统调用编号，4 表示 sys_write
-	mov ebx, 1			; 文件描述符 1 表示标准输出（stdout）
-	mov ecx, msg		; 字符串的地址
-	mov edx, 13			; 字符串的长度，包括换行符
-	int 0x80			; 发起系统调用
+	mov eax, 1			; 系统调用编号，64 位系統 1 表示 sys_write
+	mov edi, 1			; 文件描述符 1 表示标准输出（stdout）
+	mov esi, msg		; 字符串的地址
+	mov edx, length		; 字符串的长度，包括换行符
+	syscall				; 发起系统调用
 	
 	; 退出程序
-	mov eax, 1			; 系统调用编号，1 表示 sys_exit
-	xor ebx, ebx		; 返回值为 0
-	int 0x80			; 发起系统调用
+	mov eax, 60			; 系统调用编号，64 位系統 60 表示 sys_exit
+	xor edi, edi		; 返回值为 0
+	syscall				; 发起系统调用
